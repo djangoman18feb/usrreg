d3d3bbd02391165c222e90dc7ca9dc0a74783619
djangoman18feb <djangoman18feb@gmail.com>
2017-11-30 16:20:59 -0800

    Fixed Detail page access for available quotes & recepctive Author History

--- apage/views.py
+++ apage/views.py
@@ -2,23 +2,23 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from .models import Quote, About_art
+from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 
 def index(request):
     all_quotes = Quote.objects.all()
-    template = loader.get_template('apage/index.html')
     context = {
         'all_quotes': all_quotes,
     }
-
-
-
-    # html = ''
-    # for quote in all_quotes:
-    #     url = '/apage/' + str(quote.id) +'/'
-    #     html+= '<a href="' + url + '">' + quote.text + '</a><br>'
-    return HttpResponse(template.render(context, request))
+    return render(request,'apage/index.html', context)
 
 def detail(request, quote_id):
-    return HttpResponse('<h4>history of Artist of quote id' + str(quote_id) +'</h4>')
+    try:
+        aritst_history = Quote.objects.get(pk=quote_id)
+    except(Http404):
+        return HttpResponse("Page Doesn't exist")
+    else:
+        return HttpResponse('<h4>history of Artist of quote id' + str(quote_id) +'</h4>')
